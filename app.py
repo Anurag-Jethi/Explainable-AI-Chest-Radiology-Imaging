6208554ef89f6bc39c088115dd3f96e2905014c6
Anurag-Jethi <anuragjethi003@gmail.com>
2025-05-07 03:22:18 +0530

    changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from PIL import Image
 import torchvision.transforms as transforms
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -64,4 +64,5 @@ def predict():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
